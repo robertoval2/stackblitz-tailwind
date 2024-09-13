7e93d754e5ea3369722a425edbb03fe36876d526
robertoval2 <robertoval2@gmail.com>
2024-09-13 17:19:49 +0200

    Cap14-v2

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -11,8 +11,6 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
 
   return (
     <>
-       NOTE: Uncomment this code in Chapter 11 */}
-
       <div className="inline-flex">
         <PaginationArrow
           direction="left"
